4edcb6c04ed63b2e2f74c5e9fc362cca0098b191
Anderson Lima <anderson.lima@enjoytech.com.br>
2022-02-01 23:03:21 -0300

    removendo anotação

--- src/main/java/com/spring/rabbit/mq/producer/dto/Message.java
+++ src/main/java/com/spring/rabbit/mq/producer/dto/Message.java
@@ -1,12 +1,10 @@
 package com.spring.rabbit.mq.producer.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class Message {
 	
 	public Message() {

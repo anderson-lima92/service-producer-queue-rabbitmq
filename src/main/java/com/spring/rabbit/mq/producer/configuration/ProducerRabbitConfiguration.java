4a5f65199af6454a66e45156e1f5f2eff88881f7
Anderson Lima <anderson.lima@enjoytech.com.br>
2022-02-01 22:27:51 -0300

    Ajustando valor da variavel queue e exchange

--- src/main/java/com/spring/rabbit/mq/producer/configuration/ProducerRabbitConfiguration.java
+++ src/main/java/com/spring/rabbit/mq/producer/configuration/ProducerRabbitConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+ 
 /*
  *  Argumentos passados no queue() 
  *  	TRUE= quero que a fila seja duravel
